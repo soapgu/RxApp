d4749671f3b85c2927fd02371b03a830e888a5ef
guhui <guhui@shgbit.com>
2021-02-26 13:46:16 +0800

    1 only one result ,so change to Single<T> 2 add a Single<InputStream> for process http body 3 chain for download and render image

--- app/src/main/java/com/soapdemo/rxapp/MainActivity.java
+++ app/src/main/java/com/soapdemo/rxapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.soapdemo.rxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -12,10 +15,12 @@ import com.soapdemo.rxapp.models.Photo;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -25,6 +30,7 @@ import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    ImageView imageView;
     private static final OkHttpClient client = new OkHttpClient();
     private String url = "https://api.unsplash.com/photos/random?client_id=ki5iNzD7hebsr-d8qUlEJIhG5wxGwikU71nsqj8PcMM";
 
@@ -33,24 +39,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = this.findViewById((R.id.msg_view));
+        imageView = this.findViewById(R.id.imageView);
         this.findViewById(R.id.button).setOnClickListener( v -> {
             Request request = new Request.Builder()
                     .url(url)
                     .get()
                     .build();
             Call call = client.newCall(request);
+            this.textView.setText( "Finding...." );
             HttpJsonObservable(call,Photo.class)
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe( t -> this.textView.setText(t.alt_description),
-                        error -> this.textView.setText(error.getMessage())
+            .subscribe( t ->{
+                        this.textView.setText(t.alt_description);
+                        Request photoRequest = new Request.Builder()
+                                .url(t.urls.small)
+                                .get()
+                                .build();
+                        Call photoCall = client.newCall(photoRequest);
+                        this.HttpStreamObservable(photoCall)
+                                .observeOn( AndroidSchedulers.mainThread())
+                                .subscribe( s-> {
+                                    Bitmap bitmap = BitmapFactory.decodeStream(s);
+                                    s.close();
+                                    imageView.setImageBitmap(bitmap);
+                                },
+                                e -> textView.setText( e.getMessage() )
+                                );
+                    },
+                error -> this.textView.setText(error.getMessage())
             );
         } );
     }
 
-    private <T> Observable<T> HttpJsonObservable( Call call , Class<T> classOfT )
+    private  Single<InputStream> HttpStreamObservable(Call call )
     {
-        return Observable.create(subscriber -> {
-            Logger.i( "------Begin To Request Http Thread:%s",  Thread.currentThread().getId() );
+        return Single.create(subscriber -> {
+            //Logger.i( "------Begin To Request Http Thread:%s",  Thread.currentThread().getId() );
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    Logger.i("------- Http Error:%s",  e.getMessage() );
+                    subscriber.onError( e );
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) {
+                    Logger.i("------- Http Response Thread:%s",  Thread.currentThread().getId() );
+                    try{
+                        if( response.isSuccessful() ) {
+                            InputStream stream = response.body().byteStream();
+                            subscriber.onSuccess(stream);
+                        }
+                        else
+                        {
+                            subscriber.onError( new Exception( String.format( "error state code: %s", response.code() ) ) );
+                        }
+                    }
+                    catch(Exception exception) {
+                        response.close();
+                    }
+                }
+            });
+        });
+    }
+
+    private <T> Single<T> HttpJsonObservable( Call call , Class<T> classOfT )
+    {
+        return Single.create(subscriber -> {
+            //Logger.i( "------Begin To Request Http Thread:%s",  Thread.currentThread().getId() );
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(@NotNull Call call, @NotNull IOException e) {
@@ -66,10 +122,8 @@ public class MainActivity extends AppCompatActivity {
                             try {
                                 String json = body.string();
                                 Gson gson = new Gson();
-
                                 T jsonObj = gson.fromJson(json,classOfT );
-                                subscriber.onNext(jsonObj);
-                                subscriber.onComplete();
+                                subscriber.onSuccess(jsonObj);
                             } catch (IOException e) {
                                 subscriber.onError(e);
                             }
